4e86a831e50241b0502a4f8388fe35ed695c6bed
matyhtf <tianfeng.htf@alibaba-inc.com>
2013-07-09 13:11:00 +0800

    合并

--- server.php
+++ server.php
@@ -1,7 +1,9 @@
 <?php
-require(__DIR__.'/config.php');
-//$php->db->debug = true;
-//$php->tpl->debugging = true;
+define('DEBUG', 'on');
+define("WEBPATH", str_replace("\\","/", __DIR__));
+require __DIR__.'/libs/lib_config.php';
+Swoole\Config::$debug = true;
+
 $appserver = new Swoole\Network\Protocol\AppServer();
 $appserver->loadSetting(__DIR__."/swoole.ini");
 $server = new \Swoole\Network\Server('0.0.0.0', 8888);
